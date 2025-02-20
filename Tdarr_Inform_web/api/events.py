4af8125b3cdcc7e601a282b87537e61e9690190e
deathbybandaid <github@deathbybandaid.net>
2025-02-19 21:21:26 -0500

    threading

--- Tdarr_Inform_web/api/events.py
+++ Tdarr_Inform_web/api/events.py
@@ -1,7 +1,12 @@
 from flask import request, Response
 import json
+import threading
 
 import Tdarr_Inform.handlers
+
+
+def process_information(tdarr_inform, webhook_json):
+    Tdarr_Inform.handlers.Webhook_Event(tdarr_inform, webhook_json, process_event=True)
 
 
 class Events():
@@ -20,13 +25,16 @@ class Events():
         webhook_json = request.json
 
         try:
-            event_handler = Tdarr_Inform.handlers.Webhook_Event(self.tdarr_inform, webhook_json)
+            event_handler = Tdarr_Inform.handlers.Webhook_Event(self.tdarr_inform, webhook_json, process_event=False)
         except Exception as exception_msg:
             event_handler = str(exception_msg)
             self.tdarr_inform.logger.error(event_handler)
             return Response(status=501,
                             response=json.dumps({"tdarr_inform": event_handler}, indent=4),
                             mimetype='application/json')
+
+        event_handling_thread = threading.Thread(target=process_information, args=(self.tdarr_inform, webhook_json))
+        event_handling_thread.start()
 
         return Response(status=200,
                         response=json.dumps({"tdarr_inform": "success"}, indent=4),
